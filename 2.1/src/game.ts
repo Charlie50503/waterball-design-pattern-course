75c312a66f39f3eec9cd9c81caa0d60213e6ea47
charlie50503 <cate50503@gmail.com>
2023-05-28 21:37:05 +0800

    feat: 第二章節/2.1→調整 hero 攻擊所使用 method 改回 attack

--- 2.1/src/game.ts
+++ 2.1/src/game.ts
@@ -10,12 +10,12 @@ export class Game {
   }
   start(){
     while(!this.hero1.isDead() && !this.hero2.isDead()){
-      this.hero1.attackType.attack(this.hero1,this.hero2);
+      this.hero1.attack(this.hero2);
       if(this.hero2.isDead()){
         console.log("英雄" + this.hero1.name + "贏了");
         break;
       }
-      this.hero2.attackType.attack(this.hero2,this.hero1);
+      this.hero2.attack(this.hero1);
       if(this.hero1.isDead()){
         console.log("英雄" + this.hero2.name + "贏了");
         break;
--- 2.1/src/hero.ts
+++ 2.1/src/hero.ts
@@ -11,31 +11,9 @@ export class Hero {
     this.attackType = attackType
   }
 
-  // attack(hero: Hero) {
-  //   // if(attackType=="Waterball"){
-  //   //   造成一次傷害，傷害值為自己血量* 0.5
-  //   //  }esle if(attackType=="fireball"){
-  //   //   連續造成三次傷害，每次的傷害值為50
-  //   //  }else if(attackType=="earth"){
-  //   //   連續造成十次傷害，每次的傷害值為20
-  //   //  }
-  //   if (this.attackType === "WaterBall") {
-  //     hero.hp = hero.hp - this.hp * 0.5;
-  //     console.log("英雄" + this.name + "造成" + this.hp * 0.5 + "點傷害");
-  //   } else if (this.attackType === "FireBall") {
-  //     for (let index = 0; index < 3; index++) {
-  //       hero.hp -= 50;
-  //       console.log("英雄" + this.name + "造成50點傷害");
-  //     }
-
-  //   } else if (this.attackType === "Earth") {
-  //     for (let index = 0; index < 10; index++) {
-  //       hero.hp -= 20;
-  //       console.log("英雄" + this.name + "造成20點傷害");
-  //     }
-
-  //   }
-  // }
+  attack(hero:Hero){
+    this.attackType.attack(this,hero);
+  }
 
   isDead() {
     return this.hp <= 0;
