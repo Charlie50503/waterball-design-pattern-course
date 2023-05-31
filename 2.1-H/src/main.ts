2834cb5cca21d6c27f42ca9bd9c28d1b03e84f5e
charlie50503 <cate50503@gmail.com>
2023-05-31 21:36:48 +0800

    feat: 第二章節/2.1.H→添加第二個測試範例

--- 2.1-H/src/main.ts
+++ 2.1-H/src/main.ts
@@ -11,45 +11,77 @@ import { DistanceBasedCompareBehavior, HabitBasedCompareBehavior } from './app/m
 function main() {
   const matchmakingSystem = new MatchmakingSystem();
   matchmakingSystem.addIndividual(new Individual(
-    1,
-    '小明',
-    Gander.MALE,
-    18,
-    '你好',
-    new Set(['籃球','2222']),
-    new Coord(1000, 1),
-    new HabitBasedStrategy(new HabitBasedCompareBehavior(false),new CommonHabitSizeBehavior)
-  ));
-  matchmakingSystem.addIndividual(new Individual(
     2,
-    '小明2',
+    '小華',
     Gander.MALE,
-    18,
-    '你好',
-    new Set(['SSS','2222']),
-    new Coord(1, 1),
-    new HabitBasedReverseStrategy(new HabitBasedCompareBehavior(true),new CommonHabitSizeBehavior)
+    25,
+    '你好，我喜歡游泳和閱讀。',
+    new Set(['游泳', '閱讀']),
+    new Coord(2000, 2),
+    new HabitBasedStrategy(new HabitBasedCompareBehavior(true), new CommonHabitSizeBehavior)
   ));
   matchmakingSystem.addIndividual(new Individual(
     3,
-    '小明3',
-    Gander.MALE,
-    18,
-    '你好',
-    new Set(['333','籃球']),
-    new Coord(10000, 8000),
-    new DistanceBasedStrategy(new DistanceBasedCompareBehavior(false),new DistanceBehavior)
+    '小麗',
+    Gander.FEMALE,
+    22,
+    '你好，我喜歡跑步和畫畫。',
+    new Set(['跑步', '畫畫']),
+    new Coord(3000, 3),
+    new DistanceBasedReverseStrategy(new DistanceBasedCompareBehavior(true), new DistanceBehavior)
   ));
   matchmakingSystem.addIndividual(new Individual(
     4,
-    '小明4',
+    '小強',
     Gander.MALE,
-    18,
-    '你好',
-    new Set(['2222','籃球']),
-    new Coord(10000, 10000),
-    new DistanceBasedReverseStrategy(new DistanceBasedCompareBehavior(true),new DistanceBehavior)
+    28,
+    '你好，我喜歡音樂和電影。',
+    new Set(['音樂', '電影']),
+    new Coord(4000, 4),
+    new HabitBasedReverseStrategy(new HabitBasedCompareBehavior(true), new CommonHabitSizeBehavior)
   ));
+
+
+  // matchmakingSystem.addIndividual(new Individual(
+  //   1,
+  //   '小明',
+  //   Gander.MALE,
+  //   18,
+  //   '你好',
+  //   new Set(['籃球','2222']),
+  //   new Coord(1000, 1),
+  //   new HabitBasedStrategy(new HabitBasedCompareBehavior(false),new CommonHabitSizeBehavior)
+  // ));
+  // matchmakingSystem.addIndividual(new Individual(
+  //   2,
+  //   '小明2',
+  //   Gander.MALE,
+  //   18,
+  //   '你好',
+  //   new Set(['SSS','2222']),
+  //   new Coord(1, 1),
+  //   new HabitBasedReverseStrategy(new HabitBasedCompareBehavior(true),new CommonHabitSizeBehavior)
+  // ));
+  // matchmakingSystem.addIndividual(new Individual(
+  //   3,
+  //   '小明3',
+  //   Gander.MALE,
+  //   18,
+  //   '你好',
+  //   new Set(['333','籃球']),
+  //   new Coord(10000, 8000),
+  //   new DistanceBasedStrategy(new DistanceBasedCompareBehavior(false),new DistanceBehavior)
+  // ));
+  // matchmakingSystem.addIndividual(new Individual(
+  //   4,
+  //   '小明4',
+  //   Gander.MALE,
+  //   18,
+  //   '你好',
+  //   new Set(['2222','籃球']),
+  //   new Coord(10000, 10000),
+  //   new DistanceBasedReverseStrategy(new DistanceBasedCompareBehavior(true),new DistanceBehavior)
+  // ));
   matchmakingSystem.startMatch();
 }
 
