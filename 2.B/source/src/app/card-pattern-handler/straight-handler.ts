0e165fca16afeff1a2a4de52accc2b04d6a84cc5
charlie50503 <cate50503@gmail.com>
2023-07-05 11:58:43 +0800

    fix: 第二章節/2.B/CardPatternHandlerService→添加更多順子組合

--- 2.B/source/src/app/card-pattern-handler/straight-handler.ts
+++ 2.B/source/src/app/card-pattern-handler/straight-handler.ts
@@ -28,6 +28,9 @@ export class StraightHandler extends CardPatternHandler {
     const cardRanks = sortedCards.map((card) => {
       return card.rank.text;
     });
+    if(!this.cardPatternHandlerService.isNotDuplicatesNumber(cardRanks)){
+      return false;
+    }
     return straightAllPatterns.some((straightPattern) =>
       this.cardPatternHandlerService.compareArrays(
         straightPattern.combination,
@@ -39,39 +42,55 @@ export class StraightHandler extends CardPatternHandler {
 
 const straightAllPatterns = [
   {
-    combination: ['3', '4', '5', '6', '7'],
+    combination: ['A', '2', '3', '4', '5'],
     point: 1,
   },
   {
-    combination: ['4', '5', '6', '7', '8'],
+    combination: ['3', '4', '5', '6', '7'],
     point: 2,
   },
   {
-    combination: ['5', '6', '7', '8', '9'],
+    combination: ['4', '5', '6', '7', '8'],
     point: 3,
   },
   {
-    combination: ['6', '7', '8', '9', '10'],
+    combination: ['5', '6', '7', '8', '9'],
     point: 4,
   },
   {
-    combination: ['7', '8', '9', '10', 'J'],
+    combination: ['6', '7', '8', '9', '10'],
     point: 5,
   },
   {
-    combination: ['8', '9', '10', 'J', 'Q'],
+    combination: ['7', '8', '9', '10', 'J'],
     point: 6,
   },
   {
-    combination: ['9', '10', 'J', 'Q', 'K'],
+    combination: ['8', '9', '10', 'J', 'Q'],
     point: 7,
   },
   {
-    combination: ['10', 'J', 'Q', 'K', 'A'],
+    combination: ['9', '10', 'J', 'Q', 'K'],
     point: 8,
   },
   {
-    combination: ['2', '3', '4', '5', '6'],
+    combination: ['10', 'J', 'Q', 'K', 'A'],
     point: 9,
+  },
+  {
+    combination: ['J', 'Q', 'K', 'A', '2'],
+    point: 10,
+  },
+  {
+    combination: ['Q', 'K', 'A', '2', '3'],
+    point: 11,
+  },
+  {
+    combination: ['K', 'A', '2', '3', '4'],
+    point: 12,
+  },
+  {
+    combination: ['2', '3', '4', '5', '6'],
+    point: 13,
   },
 ];
