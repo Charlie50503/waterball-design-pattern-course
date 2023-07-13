72f9a45ea371275758235f996b0d77e431068970
charlie50503 <cate50503@gmail.com>
2023-07-13 11:37:20 +0800

    chore:git 設定→重新執行 git rm -rf --cached

--- c1m2s1-kata-1/dist/levelSheet.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LevelSheet = void 0;
-class LevelSheet {
-    query(exp) {
-        return exp / 1000; // 假設每等級為1000經驗值
-    }
-}
-exports.LevelSheet = LevelSheet;
